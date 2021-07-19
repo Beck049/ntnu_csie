08e8103be10bc8305c7d3066fae59eda571de10c
Beck049 <71216971+Beck049@users.noreply.github.com>
2021-07-20 02:16:37 +0800

    Create HW0101.c

---
+++ 2020_1/HW01/HW0101.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include <math.h>            
+#include <ctype.h>    
+#include <stdlib.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>	
+#include <time.h>
+
+int main()
+{
+    int id = 11;
+    char name = "Beck";
+  
+    printf("My name ID is %d.\nMy name is %s.\n", id, name);
+
+    return 0;
+}
